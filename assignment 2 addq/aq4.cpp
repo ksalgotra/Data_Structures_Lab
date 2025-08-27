4c96355a52d8e911be7ef05e8a7b8f95fad72fae
ksalgotra <ksalgotra_be24@thapar.edu>
2025-08-27 12:27:03 +0530

    aq4.cpp

--- assignment 2 addq/aq4.cpp
+++ assignment 2 addq/aq4.cpp
@@ -1,28 +1,37 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
-  public:
+public:
     void sort012(vector<int>& arr) {
         int n = arr.size();
         int l = 0;
         int r = n - 1;
-        int curr = 0;// current element i.e.
-        
-        while(curr <= r){
-            if(arr[curr] == 0){
+        int curr = 0;
+        while (curr <= r) {
+            if (arr[curr] == 0) {
                 swap(arr[l], arr[curr]);
                 l++;
                 curr++;
-            }
-            else if(arr[curr] == 2){
+            } else if (arr[curr] == 2) {
                 swap(arr[curr], arr[r]);
                 r--;
-            }
-            else{
+            } else {
                 curr++;
             }
         }
     }
 };
+
+int main() {
+    vector<int> arr = {0, 2, 1, 2, 0, 1, 0, 2};// sample testcase
+    Solution s;
+    s.sort012(arr);
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
