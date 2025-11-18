55162d17a3f9c740ac9aa2fcefd0aab980019ea6
ksalgotra <ksalgotra_be24@thapar.edu>
2025-11-19 01:15:48 +0530

    Create aq3.cpp for maxDepth

---
+++ Assignment 8 addq/aq3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
+
+int main() {
+    //creating the tree
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    root->right->right = new TreeNode(6);
+
+    Solution s;
+    cout << s.maxDepth(root) << endl;
+
+    return 0;
+}
